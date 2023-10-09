746e2deb9000e08b0e0bb4d30dcf9b64414c5d50
Lenício Jr <lenicio.junior@hotmail.com>
2023-10-09 11:40:04 -0300

    Adicionar e Listar tarefas com arquivo JSON

--- UBE231AMTDS/09-10-2023/arquivo.py
+++ UBE231AMTDS/09-10-2023/arquivo.py
@@ -4,3 +4,8 @@ import json
 def criar_arquivo(nome_arquivo, valores):
     with open(nome_arquivo, "w") as a:
         json.dump(valores, a)
+
+
+def ler_arquivo(nome_arquivo):
+    with open(nome_arquivo, "r") as a:
+        return json.load(a)
--- UBE231AMTDS/09-10-2023/main.py
+++ UBE231AMTDS/09-10-2023/main.py
@@ -1,11 +1,30 @@
 import arquivo
+from os import system
 
 if __name__ == "__main__":
     todo = []
     while True:
-        comando = input("Digite uma tarefa, ou escreva sair: ")
-        if comando.lower() == "sair":
+        system("cls")
+        print("[1] - Adicionar tarefa")
+        print("[2] - Listar tarefas")
+        print("[3] - Sair")
+        comando = input("Selecione: ")
+
+        if comando == "1":
+            todo.append(input("Insira a atividade: "))
             arquivo.criar_arquivo("todo.json", todo)
+
+        elif comando == "2":
+            dados = arquivo.ler_arquivo("todo.json")
+
+            for item in dados:
+                print(f"{item}")
+
+            input("\n\nPressione qualquer tecla para continuar...")
+
+        elif comando == "3":
             break
 
-        todo.append(comando)
+        else:
+            print("Opção inválida!")
+            input("\n\nPressione qualquer tecla para continuar...")
